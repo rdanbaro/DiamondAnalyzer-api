cfb6484be752dd79a684f8629f192a0b43875e2e
Ricardo <rdanbaro4@gmail.com>
2024-12-11 17:13:22 -0500

    Cambio de nombre de columna en servicio habito

--- services/habitos.py
+++ services/habitos.py
@@ -13,7 +13,7 @@ class HabitoService():
         data_raw = pd.read_csv(ruta)
         
         
-        data_raw = data_raw.drop(columns=['Day', 'Progress']).set_index('Date')
+        data_raw = data_raw.drop(columns=['Day', 'Progress']).set_index('Created Day')
 
         data = (
             pd.melt(data_raw, var_name='Habito', value_name='Realizado', ignore_index=False)    
@@ -23,9 +23,9 @@ class HabitoService():
 
         data = data.set_index('Habito')
         data.index.name = 'Habito'
-        data = data.sort_values(by='Date')
+        data = data.sort_values(by='Created Day')
         data = data.reset_index()
-        data.rename(columns = {'Date': 'Fecha'}, inplace = True)
+        data.rename(columns = {'Created Day': 'Fecha'}, inplace = True)
         
         data['Fecha'] = data['Fecha'].apply(convertir_fecha_a_datetime)
         
